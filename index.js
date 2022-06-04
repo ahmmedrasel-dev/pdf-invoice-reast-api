3afca44c484e75ba085e418030a00fb9dbbb9ac8
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-04 03:26:56 +0300

    create orders rest api for invoice

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ const port = process.env.PORT || 5000
 
 dotenv.config();
 
+// Connect Mongoose.
 mongoose.connect(process.env.MONGODB_URL).then(() => {
   console.log('Mongodb Connected');
 }).catch((error) => {
   console.log(error);
 })
 
+// Seeding Order Data to Databse.
 app.use('/api/seed', seedRouter);
+
+// Seeding User Data to Databse.
 app.use('/api/user-seed', userSeeder);
+
+// Orders Rest Api .
 app.use('/api/orders', orderRouter);
+
+// Get Individual Order.
+// app.use('api/order', orderRouter);
 
 app.get('/', (req, res) => {
   res.send('Surver is running')
 })
-
 
 
 app.listen(port, () => {
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,10 +3,22 @@ import Order from "../models/orderModel.js";
 
 const orderRouter = express.Router();
 
+
+// Get All Orders API:
 orderRouter.get('/', async (req, res) => {
   const orders = await Order.find();
   console.log(orders)
   res.send(orders);
 })
+
+orderRouter.get('/:id', async (req, res) => {
+  let order = await Order.findOne({ orderId: req.params.id })
+  if (order) {
+    res.send(order);
+  } else {
+    res.status(404).send({ message: 'Order is not found!' })
+  }
+})
+
 
 export default orderRouter;
